56df3a6273556d18ef49d92811646b42980e2304
asdf1899 <araid.anas99@gmail.com>
2019-02-04 02:36:53 +0100

    Add buttons

--- core/functions.php
+++ core/functions.php
@@ -1,7 +1,28 @@
 <?php
-
-  function sendMsg($token, $chatID, $msgTxt){
-    $TelegramUrlSendMessage = "https://api.telegram.org/".$token."/sendMessage?chat_id=".$chatID."&text=".urlencode($msgTxt);
+  function sendMsg($token, $chatID, $msgTxt, $button=null){
+    $keyboard = "";
+    if($button != null) {
+      $keyboard = '&reply_markup={"keyboard":['.$button.'],"resize_keyboard":true}';
+    }
+    $TelegramUrlSendMessage = "https://api.telegram.org/".$token."/sendMessage?chat_id=".$chatID."&text=".urlencode($msgTxt).$keyboard;
     return file_get_contents($TelegramUrlSendMessage);
   }
- ?>
+  function logger($MessageObj){
+    $chatID = $MessageObj['chat']['id'];
+    $senderName = $MessageObj['from']['first_name'];
+    $senderSurname = $MessageObj['from']['last_name'];
+    $senderUsername = $MessageObj['from']['username'];
+    $date =  date("d-m-Y H:i:s", $timestamp);
+    $line = "Connessione: ".$date."--".$chatID."-@".$senderUsername."-".$senderName." ".$senderSurname.'-';
+
+    $filename = "log.txt";
+    $filedir = "../$filename";
+    if (!file_exists($filedir)){
+      $log = fopen($filedir, "w");
+    }else{
+      $log = fopen($filedir, "a+");
+    }
+    fwrite($log, '$line');
+    fclose($log);
+  }
+?>
--- index.php
+++ index.php
@@ -3,20 +3,23 @@
   $botToken = "bot"."712299362:AAF5hmPddEfZNc0giZMLscjfQiQVi1y4UyE";
   $rawInput = file_get_contents("php://input");
   $update = json_decode($rawInput, TRUE);
-  print_r($update);
   if(!$update)
   {
     exit;
   }
   //Recuperiamo l'oggetto message dal json
-  $MessageObj = $update['message'];
+  $messageObj = $update['message'];
   //Recuperiamo il chatId, che utilizzeremo per rispondere all'utente che ci ha appena invocato
-  $chatID = $MessageObj['chat']['id'];
-  $sendName = $MessageObj['from']['first_name'];
-  $sendSurname = $MessageObj['from']['last_name'];
-  echo "Telegram: ".$chatID;
-  //Rispondiamo HelloWorld
-  $out = sendMsg($botToken,$chatID, "Ciao ".$sendName);
+  $chatID = $messageObj['chat']['id'];
+  $sendName = $messageObj['from']['first_name'];
+  $sendSurname = $messageObj['from']['last_name'];
 
+  logger($messageObj);
+  $text = $messageObj['text'];
+  if ($text == '/start'){
+    sendMsg($botToken,$chatID, "Benvenuto ".$sendName." se hai qualche problema con il servizio, contatta @asdf1899");
+    $buttonsCaserme = '["Btn 1" , "Btn 2"],["Test"],["Inviami"]';
+    sendMsg($botToken,$chatID, "Seleziona la tua caserma", $buttonsCaserme);
+  }
 
 ?>
