5d4413af8af401d9e18428661e04943f9ffda0e3
asdf1899 <araid.anas99@gmail.com>
2019-02-17 21:12:43 +0100

    Add section 2

--- index.php
+++ index.php
@@ -28,21 +28,43 @@
       <main class="mdl-layout__content">
         <div id="particles-js" width="100%" height="100%" style="width:100%;height:100%">
         </div>
-        <div class="mdl-grid">
-          <div class="mdl-cell mdl-cell--7-col">
-            <br><br><br><br>
-            <h2 class="style-text-red">La tua <b>caserma</b> a portata di mano...</h2>
-            <br><br>
-            <button class="style-button-red">PROVALA SUBITO!</button>
-            <button class="style-button-white">SCOPRI DI PIÙ</button>
+        <section id="home">
+          <div class="mdl-grid">
+            <div class="mdl-cell mdl-cell--7-col" style="text-align:center">
+              <br><br><br><br>
+              <h2 class="style-text-red">La tua <b>caserma</b> a portata di mano...</h2>
+              <br><br>
+              <button class="style-button-red">INIZIA SUBITO!</button>
+              <button class="style-button-white">SCOPRI DI PIÙ</button>
+            </div>
+            <div class="mdl-cell mdl-cell--5-col mdl-cell--hide-phone" style="background:url('img/abstract.svg');background-repeat:no-repeat;background-size:contain;">
+              <img src="img/screen.png" class="style-bounce" style="width:90%"></img>
+            </div>
           </div>
-          <div class="mdl-cell mdl-cell--5-col mdl-cell--hide-phone" style="background:url('img/abstract.svg');background-repeat:no-repeat;background-size:contain;">
-            <img src="img/screen.png" class="style-bounce" style="width:90%"></img>
+          <div class="style-arrow animated bounce">
+            <i class="material-icons style-arrow-icon">keyboard_arrow_down</i>
           </div>
-        </div>
-        <div class="style-arrow animated bounce">
-          <i class="material-icons style-arrow-icon">keyboard_arrow_down</i>
-        </div>
+        </section>
+        <br><br>
+        <section id="section-2">
+          <div class="mdl-grid">
+            <div class="mdl-cell mdl-cell--5-col  mdl-cell--hide-phone" style="background:url('img/abstract.svg');background-repeat:no-repeat;background-size:contain;">
+              <img src="img/server.png" class="style-bounce" style="width:90%"></img>
+            </div>
+            <div class="mdl-cell mdl-cell--7-col" style="text-align:center" >
+              <h3 class="style-text-red">Consulta e gestisci le attività in caserma in modo facile e <i>veloce.</i></h3>
+              <h5 class="style-text-darkGrey" style="text-align:left;padding:30px">
+                Una volta configurato il sistema da un tuo responsabile, non ti serve installare nulla,
+                basta avere solo un account <b>telegram</b> ed utilizzare subito il bot!
+              </h5>
+            </div>
+          </div>
+        </section>
+        <br><br>
+        <section id="section-3" style="text-align:center">
+
+
+        </section>
       </main>
     </div>
   </body>
