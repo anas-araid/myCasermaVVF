e6ed399956a79bbb9f6dbb46b83e6953358a380d
asdf1899 <araid.anas99@gmail.com>
2019-03-21 14:04:54 +0100

    Change title and date format

---
+++ app/controllers/newTurno.php
@@ -0,0 +1,42 @@
+<?php
+  @ob_start();
+  session_start();
+  include '../dbConnection.php';
+  include '../functions.php';
+  include '../getData.php';
+  include '../addData.php';
+  include '../updateData.php';
+ ?>
+<html>
+  <head>
+    <script src="../../js/script.js"></script>
+    <script src="../../js/sweetalert.js"></script>
+    <link type="text/css" rel="stylesheet" href="../../css/style.css" />
+    <link rel="stylesheet" href="../../css/font-quicksand.css">
+  </head>
+  <body>
+    <?php
+      if (!$error_message) {
+        if (isset($_POST['salva'])){
+          $idSquadra = text_filter($_POST['salva']);
+          $date = text_filter($_POST["date"]);
+          $idMezzo = text_filter($_POST["mezzo"]);
+          $addTurno = addTurno($date, $idSquadra, $idMezzo, $db_conn);
+          if ($addTurno){
+            echo "
+            <script>
+              flatAlert('', 'Turni aggiunti con successo', 'success', '../../dashboard.php?redirect=mostraSquadra&id='".$idSquadra.");
+            </script>";
+            return;
+          }else{
+            echo "
+            <script>
+              flatAlert('Errore nell\'aggiunta della squadre', 'Controlla bene i dati immessi', 'error', '../../dashboard.php?redirect=mostraSquadra&id='".$idSquadra.");
+            </script>";
+            return;
+          }
+        }
+      }
+    ?>
+  </body>
+</html>
--- app/views/_turni.php
+++ app/views/_turni.php
@@ -15,7 +15,7 @@
       }
 ?>
 <div style="text-align:center">
-  <h2 class="mdl-color-text--grey-800">Turni</h2>
+  <h2 class="mdl-color-text--grey-800">Turni - squadra <?php echo $squadraByID['Numero'] ?></h2>
   <button class="style-button-red" onclick="newTurno(<?php echo $idSquadra ?>)">AGGIUNGI TURNO</button>
   <button class="style-button-white"  onclick="location.href='?redirect=mostraSquadra&id=<?php echo $idSquadra?>'">INDIETRO</button>
 </div>
@@ -38,7 +38,7 @@
           $idMezzo = $turni[$i][3];
           $mezzo = getMezzi($idMezzo, null, $db_conn);
           echo '<tr>
-              <td class="style-td">'.$data.'</td>
+              <td class="style-td">'.date('d-m-Y', strtotime($data)).'</td>
               <td class="style-td">'.$mezzo.'</td>
               <td class="style-td"><a onclick="alertDeleteMezzo('.$id.')" style="color:red;cursor:pointer;text-decoration:underline">Elimina</a></td>
             </tr>';
@@ -63,13 +63,13 @@
     '<div class="mdl-card mdl-shadow--8dp" style="border-radius:20px;padding:20px;width:85%;min-height:200px;display:inline-block;margin:20px;text-align:center">'+
     '<h3>Nuovo turno</h3>'+
     '<br>'+
-    '<form method="post" action="app/controllers/.php" enctype="multipart/form-data">' +
+    '<form method="post" action="app/controllers/addTurni.php" enctype="multipart/form-data">' +
     '<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">'+
     '<p class="mdl-color-text--grey-800">Data</p>'+
     '<input class="mdl-textfield__input" type="date" id="date" name="date" style="outline:none" required="">'+
     '</div><br>'+
     '<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">'+
-    '<select class="mdl-textfield__input" id="grado" name="grado" required="" style="outline:none">'+
+    '<select class="mdl-textfield__input" id="mezzo" name="mezzo" required="" style="outline:none">'+
     <?php
       $mezzi = getMezzi(null, $_SESSION['ID'], $db_conn);
       $selected = '';
