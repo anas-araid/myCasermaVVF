77d4c13bb62bd8dfeda6125814ced35519755380
asdf1899 <araid.anas99@gmail.com>
2019-03-22 17:41:48 +0100

    Fix padding and tingle margin

--- app/views/_home.php
+++ app/views/_home.php
@@ -1,5 +1,5 @@
-<div style="text-align:center">
-  <br><br><br><br>
+<div style="text-align:center;padding:0% 5%">
+  <br><br><br>
   <h2 class="mdl-color-text--grey-800">Benvenuto su <span class="style-text-red" style="font-weight:100">my</span><span class="style-text-red" style="font-weight:500">Caserma</span><span class="style-text-red" style="font-weight:600">VVF</span></h2>
   <br><br><br><br>
   <h3 class="mdl-color-text--grey-800">Tramite il menu laterale è possibile configurare tutte le risorse del corpo:</h3>
