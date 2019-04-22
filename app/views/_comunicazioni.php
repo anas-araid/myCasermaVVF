1e07035428ef570acf6f4fff84d05aac41b434f4
asdf1899 <araid.anas99@gmail.com>
2019-04-22 21:15:50 +0200

    Fix form

--- app/views/_comunicazioni.php
+++ app/views/_comunicazioni.php
@@ -3,14 +3,17 @@
   <br>
   <form action="app/controllers/" method="POST" style="text-align:center">
     <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="messaggio" name="messaggio" required="">
-      <label class="mdl-textfield__label" for="caserma">Invia un messaggio a tutti i vigili</label>
+      <textarea class="mdl-textfield__input" row="3" id="messaggio" name="messaggio" required="" maxlength="40"></textarea>
+      <label class="mdl-textfield__label" for="messaggio">Invia un messaggio a tutti i vigili</label>
     </div>
     <br>
     <div>
       <button class="style-button-red" 
-              name="invia"
-              onclick="flatAlert('Vuoi continuare?', 'Tutti i vigili riceveranno il messaggio', 'warning', 'submit', true)" >INVIA</button><br>
+              type="submit"
+              value="<?php echo $caserma['ID'] ?>"
+              name="invia">
+        INVIA
+      </button><br>
       <button class="style-button-white" onclick="location.href='?redirect=home'" type="reset">INDIETRO</button>
     </div>
     </div>
