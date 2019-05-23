559aa5e11b2eff8157fb8933ebc54882c054aec8
asdf1899 <araid.anas99@gmail.com>
2019-05-23 11:43:22 +0200

    Fix viewport

--- app/controllers/newMessaggio.php
+++ app/controllers/newMessaggio.php
@@ -10,6 +10,7 @@
  ?>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="../../js/script.js"></script>
     <script src="../../js/sweetalert.js"></script>
     <link type="text/css" rel="stylesheet" href="../../css/style.css" />
